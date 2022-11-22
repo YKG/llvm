b902dfca143237fe4f2406ea53e08069227216bd
Kaige Ye <ye@kaige.org>
2022-11-22 09:17:35 +0800

    remove code

--- llvm/lib/Target/YCore/YCoreMCInstLower.cpp
+++ llvm/lib/Target/YCore/YCoreMCInstLower.cpp
@@ -28,52 +28,6 @@ YCoreMCInstLower::YCoreMCInstLower(class AsmPrinter &asmprinter)
 
 void YCoreMCInstLower::Initialize(MCContext *C) { Ctx = C; }
 
-MCOperand YCoreMCInstLower::LowerSymbolOperand(const MachineOperand &MO,
-                                               MachineOperandType MOTy,
-                                               unsigned Offset) const {
-  MCSymbolRefExpr::VariantKind Kind = MCSymbolRefExpr::VK_None;
-  const MCSymbol *Symbol;
-
-  switch (MOTy) {
-    case MachineOperand::MO_MachineBasicBlock:
-      Symbol = MO.getMBB()->getSymbol();
-      break;
-    case MachineOperand::MO_GlobalAddress:
-      Symbol = Printer.getSymbol(MO.getGlobal());
-      Offset += MO.getOffset();
-      break;
-    case MachineOperand::MO_BlockAddress:
-      Symbol = Printer.GetBlockAddressSymbol(MO.getBlockAddress());
-      Offset += MO.getOffset();
-      break;
-    case MachineOperand::MO_ExternalSymbol:
-      Symbol = Printer.GetExternalSymbolSymbol(MO.getSymbolName());
-      Offset += MO.getOffset();
-      break;
-    case MachineOperand::MO_JumpTableIndex:
-      Symbol = Printer.GetJTISymbol(MO.getIndex());
-      break;
-    case MachineOperand::MO_ConstantPoolIndex:
-      Symbol = Printer.GetCPISymbol(MO.getIndex());
-      Offset += MO.getOffset();
-      break;
-    default:
-      llvm_unreachable("<unknown operand type>");
-  }
-
-  const MCSymbolRefExpr *MCSym = MCSymbolRefExpr::create(Symbol, Kind, *Ctx);
-
-  if (!Offset)
-    return MCOperand::createExpr(MCSym);
-
-  // Assume offset is never negative.
-  assert(Offset > 0);
-
-  const MCConstantExpr *OffsetExpr =  MCConstantExpr::create(Offset, *Ctx);
-  const MCBinaryExpr *Add = MCBinaryExpr::createAdd(MCSym, OffsetExpr, *Ctx);
-  return MCOperand::createExpr(Add);
-}
-
 MCOperand YCoreMCInstLower::LowerOperand(const MachineOperand &MO,
                                          unsigned offset) const {
   MachineOperandType MOTy = MO.getType();
@@ -86,13 +40,6 @@ MCOperand YCoreMCInstLower::LowerOperand(const MachineOperand &MO,
       return MCOperand::createReg(MO.getReg());
     case MachineOperand::MO_Immediate:
       return MCOperand::createImm(MO.getImm() + offset);
-    case MachineOperand::MO_MachineBasicBlock:
-    case MachineOperand::MO_GlobalAddress:
-    case MachineOperand::MO_ExternalSymbol:
-    case MachineOperand::MO_JumpTableIndex:
-    case MachineOperand::MO_ConstantPoolIndex:
-    case MachineOperand::MO_BlockAddress:
-      return LowerSymbolOperand(MO, MOTy, offset);
     case MachineOperand::MO_RegisterMask:
       break;
   }
